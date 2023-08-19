46694f7013c0ae344a2a8fe3c16f535ce529ffd8
hxy1991 <huangxiaoyu1018@gmail.com>
2023-08-19 14:44:00 +0800

    [http] refactor header util

--- http/header_util.go
+++ http/header_util.go
@@ -2,30 +2,34 @@ package http
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/hxy1991/sdk-go/log"
 	"os"
 )
 
 var serverEnv = os.Getenv("SERVER_ENV")
 
-func BuildHeaderForRoute(ctx context.Context, clientData map[string]interface{}, service string) map[string]string {
+func BuildHeaderForRoute(ctx context.Context, service, clientDataStr, channelId string) map[string]string {
 	headers := map[string]string{}
 	headers["Service"] = service
 	headers["Env"] = serverEnv
 
-	appVersion, ok := clientData["appVersion"]
+	clientDataMap := map[string]interface{}{}
+	if clientDataStr != "" {
+		err := json.Unmarshal([]byte(clientDataStr), &clientDataMap)
+		if err != nil {
+			log.Context(ctx).Warnf("clientData json.Unmarshal err %+v", err)
+		}
+	}
+
+	appVersion, ok := clientDataMap["appVersion"]
 	if ok {
 		headers["Client-Version"] = appVersion.(string)
 	} else {
 		log.Context(ctx).Warn("appVersion is not exist")
 	}
 
-	channelId, ok := clientData["Channel-Id"]
-	if ok {
-		headers["Channel-Id"] = channelId.(string)
-	} else {
-		log.Context(ctx).Warn("channelId is not exist")
-	}
+	headers["Channel-Id"] = channelId
 
 	return headers
 }
