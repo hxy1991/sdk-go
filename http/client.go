4eae95d0f94b361e1d06ba6397cb64a091c90071
hxy1991 <huangxiaoyu1018@gmail.com>
2023-08-19 21:24:53 +0800

    [log] requestMethod

--- http/client.go
+++ http/client.go
@@ -80,7 +80,7 @@ func SendWithTimeout(ctx context.Context, url, method string, requestBody []byte
 		duration := time.Now().Sub(startTime)
 		log.Context(ctx).
 			With("requestPath", url).
-			With("method", method).
+			With("requestMethod", method).
 			With("responseCode", responseCode).
 			With("requestBody", string(requestBody), "responseBody", string(responseBody)).
 			With("requestHeader", headers).
