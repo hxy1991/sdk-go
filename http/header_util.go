3e564fcc055067c8ce622b537f6070eb1760f169
hxy1991 <huangxiaoyu1018@gmail.com>
2023-08-19 15:06:16 +0800

    [http] refactor header util

--- http/header_util.go
+++ http/header_util.go
@@ -4,31 +4,40 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/hxy1991/sdk-go/log"
+	"net/http"
 	"os"
 )
 
 var serverEnv = os.Getenv("SERVER_ENV")
 
-func BuildHeaderForRoute(ctx context.Context, service, clientDataStr, channelId string) map[string]string {
+func BuildHeaderForRoute(ctx context.Context, service string, header http.Header) map[string]string {
 	headers := map[string]string{}
 	headers["Service"] = service
 	headers["Env"] = serverEnv
 
-	clientDataMap := map[string]interface{}{}
-	if clientDataStr != "" {
-		err := json.Unmarshal([]byte(clientDataStr), &clientDataMap)
-		if err != nil {
-			log.Context(ctx).Warnf("clientData json.Unmarshal err %+v", err)
+	clientVersion := header.Get("Client-Version")
+	if len(clientVersion) == 0 {
+		clientDataStr := header.Get("Clientdata")
+		clientDataMap := map[string]interface{}{}
+		if clientDataStr != "" {
+			err := json.Unmarshal([]byte(clientDataStr), &clientDataMap)
+			if err != nil {
+				log.Context(ctx).Warnf("clientData json.Unmarshal err %+v", err)
+			}
+
+			appVersion, ok := clientDataMap["appVersion"]
+			if ok {
+				clientVersion = appVersion.(string)
+			}
 		}
 	}
+	headers["Client-Version"] = clientVersion
 
-	appVersion, ok := clientDataMap["appVersion"]
-	if ok {
-		headers["Client-Version"] = appVersion.(string)
-	} else {
-		log.Context(ctx).Warn("appVersion is not exist")
+	channelId := header.Get("Channel-Id")
+	if len(channelId) == 0 {
+		// 1-ios, 2-android, 0-其他
+		channelId = "0"
 	}
-
 	headers["Channel-Id"] = channelId
 
 	return headers
